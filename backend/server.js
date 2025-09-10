8599bdc6464227f4352a2892ae6a4cee6e0fb16c
rupeshchy10 <rupevilary1010@gmail.com>
2025-09-10 19:02:09 +0545

    refactor: implement product retrieval and creation functionality

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cors from "cors";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -16,6 +17,35 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet()); //Helmet is a security middleware that helps you protect your app by setting various HTTP headers
 app.use(morgan("dev")); //log the requests
+
+//apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+	try {
+		const decision = await aj.protect(req, {
+			requested: 1, //specifies that each request consumes 1 token
+		});
+		if (decision.isDenied()) {
+			if (decision.reason.isRateLimit()) {
+				res.status(429).json({ error: "Too Many Requests" });
+			} else if (decision.reason.isBot()) {
+				res.status(403).json({ error: "Bot access denied" });
+			} else {
+				res.status(403).json({ error: "Forbidden" });
+			}
+			return;
+		}
+		//check for spoofed bots
+		if (
+			decision.results.some(
+				(result) => result.reason.isBot() && result.reason.isSpoofed()
+			)
+		) {
+			res.status(403).json({ error: "Spoofed bot detected" });
+			return;
+		}
+		next();
+	} catch (error) {}
+});
 
 app.use("/api/products", productRoutes);
 
@@ -35,7 +65,6 @@ async function initDB() {
 	}
 }
 
-console.log("Database Connected Successfully")
 initDB().then(() => {
 	app.listen(PORT, () => {
 		console.log(`Server is running on port ${PORT}`);
